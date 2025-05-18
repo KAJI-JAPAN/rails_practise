2c1c99867e496f26693dfff862a12d92799f1dff
KAJI-JAPAN <kurowassan1206@gmail.com>
2025-05-19 00:00:30 +0900

    追加

--- app/architecture/strategy/202505011_strategy.rb
+++ app/architecture/strategy/202505011_strategy.rb
@@ -9,29 +9,87 @@
 class BasePayment
   def initialize(card_info)
     unless card_info
-      pp "有効なカード情報がありません"
+      raise "有効な支払い情報がありません"
     end
+    @card_info = card_info
   end
-
+  def pay
+    pp "支払いを行いました"
+  end
 end
 
 # -------
-class CardPayment
-  
+class CardPayment < BasePayment
+  def initialize(card_info)
+    unless card_info
+      raise "有効なカード情報がありません"
+    end
+    @card_info = card_info
+  end
+
+  def pay
+    pp "cardで支払いを行いました"
+  end
 end
 
 # ---------
 
-class BankTransfer
+class BankTransfer < BasePayment
+  def initialize(card_info)
+    unless card_info
+      raise "有効な決済情報ではありません"
+    end
+    @card_info = card_info
+  end
 
+  def pay
+    pp ""
+  end
 end
 
 #----------
 
 
-class ElectronicMoneyPayment
-
+class ElectronicMoneyPayment < BasePayment
+  def initialize(card_info)
+    unless card_info
+      raise "電子マネーに関連するエラーです"
+    end
+    @card_info = card_info
+  end
 end
+
+class PaymentStrategySelector
+  def self.build(type, info)
+    case type
+    when :card
+      CardPayment.new(info)
+    when :bank
+    BankTransfer.new(info)
+    when :electronicMoneyPayment
+      ElectronicMoneyPayment.new(info)
+    else
+      raise "Unknown payment type: #{type}"
+    end
+  end
+end
+
+class PaymentProccessor
+  def initialize(payment)
+    @strategy = payment
+  end
+
+  def pay
+    @strategy.pay
+  end
+end
+
+# クレジットカード払いを選ぶ
+card_strategy = PaymentStrategySelector.build(:card, "**** **** **** 1234")
+PaymentProcessor.new(card_strategy).pay
+
+
+
 
 #----------
 
